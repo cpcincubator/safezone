97e08a1dc88cb54b341badf0d2d89bd770d583f6
shudipto <shudipta15-11424@diu.edu.bd>
2020-11-19 22:12:55 +0600

    initial commit

---
+++ report/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'report/home.html')
+
+def submit(request):
+    return render(request,'report/submit.html')
+
+def read(request):
+    return render(request,'report/read.html')
